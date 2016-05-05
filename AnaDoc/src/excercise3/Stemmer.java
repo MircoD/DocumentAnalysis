fcbd76b1b6048a24ad5d7b94a794f9d154d3db27
Maurice Isele <Maurice.Isele@gmx.de>
2016-05-05 12:44:36 +0200

    Implemented first rules for the stemmer.
    Seems to be working well so far. Need to add the remaining rules, and handle irregular cases like 'does'.

--- AnaDoc/src/excercise3/Stemmer.java
+++ AnaDoc/src/excercise3/Stemmer.java
@@ -3,6 +3,7 @@ package excercise3;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.*;
 
 import edu.mit.jwi.Dictionary;
 import edu.mit.jwi.IDictionary;
@@ -32,28 +33,131 @@ public class Stemmer {
 
 	}
 
+	/*
+	 * --------------------------------------------------------
+	 * TO DO: SOME MORE RULES + IRREGULARS LIKE 'DOES' -> 'DO'
+	 * --------------------------------------------------------
+	 * 
+	 * Already covers cases like the following (use for testing):
+	 * 
+	 * Stemmer stemmer = new Stemmer(); String[] tokens = { "gladly",
+	 * "computer", "windy", "dusty", "stronger", "merciless", "foggy",
+	 * "surely", "happily"}; tokens = stemmer.stem(tokens);
+	 */
 	public String[] stem(String[] tokens) {
+
+		IDictionary dict = null;
+
+		try {
+			// construct the URL to the Wordnet dictionary directory
+			String path = "C:/Users/Maurice/Desktop/Studium/6. Semester/CMDA/wn3.1.dict/dict";
+			URL url = new URL("file", null, path);
+
+			// construct the dictionary object and open it
+			dict = new Dictionary(url);
+			dict.open();
+		} catch (IOException e) {
+			System.out.println("Accessing dictionary failed: " + e);
+			return tokens;
+		}
 
 		for (int i = 0; i < tokens.length; i++) {
 
-			// Suffix -s
-			if (tokens[i].matches("[A-Z,a-z]+s")) {
-				tokens[i] = tokens[i].replaceAll("s(?=\\s+|$)", "");
-				i--;
+			// Suffix -less
+			if (tokens[i].matches("[A-Za-z\\-]+less")) {
+				String tmp = tokens[i].replaceAll("less(?=\\s+|$)", "");
+				// cases like mercy -> merciless
+				if (tmp.matches("[A-Za-z\\-]+i")) {
+					tmp = tmp.replaceAll("i(?=\\s+|$)", "y");
+				}
+				// check whether tmp exists as a word
+				if (exists(dict, tmp)) {
+					tokens[i] = tmp;
+					i--;
+				}
 			}
 
-			// Suffix -er
-			if (tokens[i].matches("[A-Z,a-z]+er")) {
-				tokens[i] = tokens[i].replaceAll("er(?=\\s+|$)", "e");
-				i--;
+			// Suffix -ness
+			if (tokens[i].matches("[A-Za-z\\-]+ness")) {
+				String tmp = tokens[i].replaceAll("ness(?=\\s+|$)", "");
+				// cases like manliness -> manly
+				if (tmp.matches("[A-Za-z\\-]+i")) {
+					tmp = tmp.replaceAll("i(?=\\s+|$)", "y");
+				}
+				// check whether tmp exists as a word
+				if (exists(dict, tmp)) {
+					tokens[i] = tmp;
+					i--;
+				}
 			}
 
-			// etc., additionally add dictionary queries
+			// Suffix -er ; Rule 1 (e.g. stronger -> strong)
+			if (tokens[i].matches("[A-Za-z\\-]+er")) {
+				String tmp = tokens[i].replaceAll("er(?=\\s+|$)", "");
+				// check whether tmp exists as a word
+				if (exists(dict, tmp)) {
+					tokens[i] = tmp;
+					i--;
+				}
+			}
 
+			// Suffix -er ; Rule 2 (e.g. computer -> compute)
+			if (tokens[i].matches("[A-Za-z\\-]+er")) {
+				String tmp = tokens[i].replaceAll("er(?=\\s+|$)", "e");
+				// check whether tmp exists as a word
+				if (exists(dict, tmp)) {
+					tokens[i] = tmp;
+					i--;
+				}
+			}
+
+			// Suffix -y
+			if (tokens[i].matches("[A-Za-z\\-]+y")) {
+				String tmp = tokens[i].replaceAll("y(?=\\s+|$)", "");
+				// cases like foggy -> fog
+				if (tmp.length() > 2) {
+					tmp = tmp.substring(0, tmp.length() - 2)
+							+ tmp.substring(tmp.length() - 2).replaceAll(
+									"([A-Za-z\\-])\\1", "$1");
+				}
+				// check whether tmp exists as a word
+				if (exists(dict, tmp)) {
+					tokens[i] = tmp;
+					i--;
+				}
+			}
+
+			// Suffix -ly
+			if (tokens[i].matches("[A-Za-z\\-]+ly")) {
+				String tmp = tokens[i].replaceAll("ly(?=\\s+|$)", "");
+				// cases like happily -> happy
+				if (tmp.matches("[A-Za-z\\-]+i")) {
+					tmp = tmp.replaceAll("i(?=\\s+|$)", "y");
+				}
+				// check whether tmp exists as a word
+				if (exists(dict, tmp)) {
+					tokens[i] = tmp;
+					i--;
+				}
+			}
 		}
-
 
 		return tokens;
 	}
 
+	/*
+	 * Checks, whether the specified word can be found in the dictionary.
+	 * 
+	 * @param IDictionary - the dictionary to be used
+	 * 
+	 * @param String - the word to be checked
+	 * 
+	 * @return Boolean - whether the word could be found
+	 */
+	private boolean exists(IDictionary dict, String word) {
+		return (dict.getIndexWord(word, POS.NOUN) != null
+				|| dict.getIndexWord(word, POS.VERB) != null
+				|| dict.getIndexWord(word, POS.ADJECTIVE) != null || dict
+					.getIndexWord(word, POS.ADVERB) != null);
+	}
 }
