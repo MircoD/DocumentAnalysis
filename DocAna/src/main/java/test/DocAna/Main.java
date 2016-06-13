2655ccd99ab8ec153c23ce78efe61685e6e84b4f
MircoD <mirco.dietrich@uni-konstanz.de>
2016-06-13 17:21:12 +0200

    fixed TermFrequ

--- DocAna/src/main/java/test/DocAna/Main.java
+++ DocAna/src/main/java/test/DocAna/Main.java
@@ -1,31 +1,28 @@
 package test.DocAna;
 
-import java.util.Arrays;
-
-
-
+import java.util.ArrayList;
 
 /*
  B002LBKDYE
-B004WO6BPS
-B009NQKPUW
-B000VBJEFK
-7883704540
-B0028OA3EY
-B0028OA3EO
-B008PZZND6
-B006TTC57C
-B002VL2PTU
-B001NFNFMQ
-B000067JG3
-B000067JG4
-B000MMMTAK
-B003DBEX6K
-B001TAFCBC
-B0039UTDFG
-B000KKQNRO
-B0002Y69NQ
-B005CA4SJW
+ B004WO6BPS
+ B009NQKPUW
+ B000VBJEFK
+ 7883704540
+ B0028OA3EY
+ B0028OA3EO
+ B008PZZND6
+ B006TTC57C
+ B002VL2PTU
+ B001NFNFMQ
+ B000067JG3
+ B000067JG4
+ B000MMMTAK
+ B003DBEX6K
+ B001TAFCBC
+ B0039UTDFG
+ B000KKQNRO
+ B0002Y69NQ
+ B005CA4SJW
  */
 /**
  * Main class for the pipelining.
@@ -36,14 +33,40 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//POSTagger tagger = new POSTagger();
-		//tagger.importAndCountCorpus();
-		//Gui gui = new Gui(tagger);
-		
+		// POSTagger tagger = new POSTagger();
+		// tagger.importAndCountCorpus();
+		// Gui gui = new Gui(tagger);
+
+		ArrayList<Review> listOfReviews = new ArrayList<Review>();
+		ArrayList<Movies> listOfMovies = new ArrayList<Movies>();
 		Reader reader = new Reader();
-		reader.readAndClear("e://Downloads/docAnaTextSample.rtf");
+		TermFrequency tf = new TermFrequency();
+		listOfReviews = reader
+				.readAndClear("e://Downloads/docAnaTextSample.rtf");
+
+		System.out.println("reader done");
+		for (int i = 0; i < listOfReviews.size(); i++) {
+			
+			boolean newMovie =true;
+
+			for (int j = 0; j < listOfMovies.size() && newMovie; j++) {
+				if (listOfMovies.get(j).movieID.compareTo(listOfReviews.get(i).getProductId()) == 0) {
+					Movies tmpMovie = listOfMovies.get(j);
+					ArrayList<Review> tmp = tmpMovie.getReviews();
+					tmp.add(listOfReviews.get(i));
+					tmpMovie.setReviews(tmp);
+					listOfMovies.set(j, tmpMovie);	
+					newMovie=false;
+				}
+			}
+			
+			if(newMovie){				
+				listOfMovies.add(new Movies(listOfReviews.get(i).getProductId(), listOfReviews.get(i) ));
+			}
+		}
 		
+		tf.count(listOfMovies);
 		
-		
+
 	}
 }
--- DocAna/src/main/java/test/DocAna/TermFrequency.java
+++ DocAna/src/main/java/test/DocAna/TermFrequency.java
@@ -2,39 +2,46 @@ package test.DocAna;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class TermFrequency {
-	HashMap<String, Integer> words;
-	ArrayList<ArrayList<Integer>> frequencies;
-	
+
 	public void count(ArrayList<Movies> movies){
 		
+		Logger log = new Logger();
+		ArrayList<ArrayList<Integer>> frequencyCountMatrix = new ArrayList<ArrayList<Integer>>();
+		HashMap<String, Integer> words = new HashMap<String, Integer>();
+		
+		for(int i=0;i<movies.size();i++){
+			frequencyCountMatrix.add(new ArrayList<Integer>());
+		}
+		
+		//loop through all movies
 		for(int i =0; i < movies.size();i++){
 			ArrayList<Review> currentListOfReview = movies.get(i).getReviews();
 			
+			//loop through all reviews of a movie
 			for(int j=0;j<currentListOfReview.size();j++){
 				String[] currentListOfTokens =currentListOfReview.get(j).getText();
+				
+				//loop through all words of a review
 				for(int k=0; k<currentListOfTokens.length;k++){
 					String currentToken =  currentListOfTokens[k];
-					
-					if(words.containsKey(currentListOfTokens[k])){
+
+					if(words.containsKey(currentToken)){
 						int index= words.get(currentToken);
-						ArrayList<Integer> tmp = frequencies.get(i);
+						ArrayList<Integer> tmp = frequencyCountMatrix.get(i);
 						tmp.set(index, tmp.get(index)+1); 
-						frequencies.set(i, tmp);
+						frequencyCountMatrix.set(i, tmp);
 					}
-					else{
+					else {
 						for(int l =0; l < movies.size();l++){
-							ArrayList<Integer> tmp1 = frequencies.get(l);
-							tmp1.add(0);
-							frequencies.set(l, tmp1);
+							frequencyCountMatrix.get(l).add(0);
 						}
 						
-						ArrayList<Integer> tmp = frequencies.get(i);
+						ArrayList<Integer> tmp = frequencyCountMatrix.get(i);
 						tmp.set(tmp.size()-1, 1);
-						words.put(currentToken, frequencies.get(0).size());
-						
-						
+						words.put(currentToken, frequencyCountMatrix.get(0).size()-1);							
 					}
 					
 				}
@@ -42,6 +49,24 @@ public class TermFrequency {
 			
 		}
 		
+		int amountWords =words.size();
+		System.out.println(amountWords);
+			
+		Iterator itr = words.keySet().iterator();
+		while(itr.hasNext()){
+			String tmp123 = itr.next().toString();
+			log.log(tmp123 + " "+ words.get(tmp123) , "words");
+			
+		}
+		
+		for(int i=0; i< movies.size();i++){
+			System.out.println(frequencyCountMatrix.get(i).size());
+			ArrayList<Review> currentListOfReview = movies.get(i).getReviews();
+			String tmp = movies.get(i).movieID;
+			for(int j=0;j<amountWords;j++){
+			log.log(frequencyCountMatrix.get(i).get(j).toString(), tmp);
+			}
+		}
 		
 	}
 }
